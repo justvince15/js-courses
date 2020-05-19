74e41d6a8a7bbc277fcc412999422d0d0919ed32
justvince15 <justvince15@gmail.com>
2020-05-19 23:20:00 +0300

    Modified to use filter for getting unique skills

--- lesson-06/homework-06/task-10.js
+++ lesson-06/homework-06/task-10.js
@@ -1,48 +1,51 @@
+/* eslint-disable no-shadow */
+
 // Получить массив всех умений всех пользователей (поле skills), при этом не должно быть
 // повторяющихся умений и они должны быть отсортированы в алфавитном порядке.
 
 // eslint-disable-next-line import/extensions
 import users from './users.js';
 
-// eslint-disable-next-line no-shadow
 const getSortedUniqueSkills = users => {
   const allSkills = users.reduce((skills, user) => {
     skills.push(...user.skills);
     return skills;
   }, []);
 
-  const allUniqueSkills = allSkills.reduce((uniqueSkills, skill) => {
-    if (!uniqueSkills.includes(skill)) {
-      uniqueSkills.push(skill);
-    }
-    return uniqueSkills;
-  }, []);
+  // Способ 1.1
+  const allUniqueSkills = allSkills.filter(
+    (skill, index, array) => array.indexOf(skill) === index,
+  );
 
   return allUniqueSkills.sort();
+
+  // Способ 1.2 - без промежуточного вычисления allUniqueSkills:
+  // return allSkills
+  //   .filter((skill, index, array) => array.indexOf(skill) === index)
+  //   .sort();
 };
 
-// eslint-disable-next-line no-shadow
 const getSortedUniqueSkills2 = users => {
-  // eslint-disable-next-line no-shadow
   const getAllSkills = users =>
     users.reduce((skills, user) => {
       skills.push(...user.skills);
       return skills;
     }, []);
 
+  // Способ 2.1
   const getAllUniqueSkills = allSkills =>
-    allSkills.reduce((uniqueSkills, skill) => {
-      if (!uniqueSkills.includes(skill)) {
-        uniqueSkills.push(skill);
-      }
-      return uniqueSkills;
-    }, []);
+    allSkills.filter((skill, index, array) => array.indexOf(skill) === index);
 
   return getAllUniqueSkills(getAllSkills(users)).sort();
+
+  // Способ 2.2 - без промежуточного использования getAllUniqueSkills():
+  // return getAllSkills(users)
+  //   .filter((skill, index, array) => array.indexOf(skill) === index)
+  //   .sort();
 };
 
 console.log(getSortedUniqueSkills(users));
-// [ 'adipisicing', 'amet', 'anim', 'commodo', 'culpa', 'elit', 'ex', 'ipsum', 'irure', 'laborum', 'lorem', 'mollit', 'non', '
+// ["adipisicing", "amet", "anim", "commodo", "culpa", "elit", "ex", "ipsum", "irure", "laborum", "lorem", "mollit", "non", "nostrud", "nulla", "proident", "tempor", "velit", "veniam"]
 
 console.log(getSortedUniqueSkills2(users));
-// [ 'adipisicing', 'amet', 'anim', 'commodo', 'culpa', 'elit', 'ex', 'ipsum', 'irure', 'laborum', 'lorem', 'mollit', 'non', '
+// ["adipisicing", "amet", "anim", "commodo", "culpa", "elit", "ex", "ipsum", "irure", "laborum", "lorem", "mollit", "non", "nostrud", "nulla", "proident", "tempor", "velit", "veniam"]
